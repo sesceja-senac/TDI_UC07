6964eec6f60b4e91876463ece6e6d1e868654f9a
ymbitancourt <ymbitancourt@senacrs.com.br>
2023-08-08 01:09:47 -0300

    conteudo 10

--- 10_briefing/js/custom.js
+++ 10_briefing/js/custom.js
@@ -51,3 +51,54 @@ $(".btn-exercicio07").click(function(){
     $(this).addClass("resposta-certa");
     FXManager.snd_certo.play();
 });
+
+
+
+
+
+
+//subir pagina ao expandir um painel
+$('.collapse').on('shown.bs.collapse', function () {               
+    $('html, body').animate({scrollTop:$(this).offset().top-120},500);
+  });
+  
+  
+  
+  // Animação de scrolling
+  window.addEventListener('load', function(){
+    console.log('iniciando no load')
+  })
+  
+  // animação scroll no menu
+  $('.indiceLista a').on('click', function() {
+    $("html, body").animate({scrollTop: $('#'+$(this).data('section')).offset().top-80}, 800);
+  });
+  
+  var alturas = {};
+  $('.ancora').each(function () {
+    alturas[$(this).prop('id')] = $(this).offset().top; 
+  });
+  
+  $(window).on('scroll', function() {
+    for(var seccao in alturas) {
+      if($(window).scrollTop() >= alturas[seccao]) {
+        $('.indiceLista a').removeClass('active'); 
+        $('.indiceLista a[data-section="' +seccao+ '"]').addClass('active'); 
+      }
+    }
+  });
+  
+  
+  $(function() {
+    $(window).scroll(function(){
+      var topo = $('#topo').height(); // altura do topo
+      var scrollTop = $(window).scrollTop(); // qto foi rolado a barra
+      var tamPagina = $(document).height(); // altura da página
+        
+      if(scrollTop > topo){
+        $('#scrollSpyNav').css({'top' : 0});
+      }else{
+        $('#scrollSpyNav').css({'top' : 100});
+      }               
+    });
+  });
